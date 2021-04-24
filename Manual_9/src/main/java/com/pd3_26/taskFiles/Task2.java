fa50e75b23de01a67e73b340d6775fdd12d60809
BlueRamus <moizwajahat3@gmail.com>
2021-04-25 02:52:43 +0500

    Changed once more

--- Manual_9/src/main/java/com/pd3_26/taskFiles/Task2.java
+++ Manual_9/src/main/java/com/pd3_26/taskFiles/Task2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pd3_26.task1.T2;
+package com.pd3_26.taskFiles;
 
 import javax.swing.JOptionPane;
 
