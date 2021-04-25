2926e19c107a9735d38ae02fac639feb0b211393
BlueRamus <moizwajahat3@gmail.com>
2021-04-25 16:37:09 +0500

    Update Task4.java

--- Manual_9/src/main/java/com/pd3_26/taskFiles/Task4.java
+++ Manual_9/src/main/java/com/pd3_26/taskFiles/Task4.java
@@ -10,7 +10,57 @@ package com.pd3_26.taskFiles;
  * @author DELL
  */
 public class Task4 {
-    static String RetainIntegers (String S) {
+    public static void main (String[] args) {
         
+    }
+}
+
+class MyString {
+    static String RetainIntegers (String S) {
+        String IntOnly = "";
+        
+        for (int i = 0; i < S.length(); i++)
+        {
+            if ( (S.charAt(i) >= '0') && (S.charAt(i) <= '9') )
+            {
+                IntOnly = IntOnly + S.charAt(i);
+            }
+        }
+        
+        return IntOnly;
+    }
+    
+    static String RemovePuncs (String S) {
+        String NoPunc = "";
+        
+        for (int i = 0; i < S.length(); i++)
+        {
+            if ( !((S.charAt(i) == '!') || (S.charAt(i) == '\"') || (S.charAt(i) == '\'')) )
+            {
+                NoPunc = NoPunc + S.charAt(i);
+            }
+            else if ( !((S.charAt(i) == '(') || (S.charAt(i) == ')') || (S.charAt(i) == ',')) )
+            {
+                NoPunc = NoPunc + S.charAt(i);
+            } 
+            else if ( !((S.charAt(i) == '-') || (S.charAt(i) == '.') || (S.charAt(i) == '/')) )
+            {
+                NoPunc = NoPunc + S.charAt(i);
+            }
+            else if ( !((S.charAt(i) == ':') || (S.charAt(i) == ';') || (S.charAt(i) == '?')) )
+            {
+                NoPunc = NoPunc + S.charAt(i);
+            }
+            else if ( !((S.charAt(i) == '[') || (S.charAt(i) == '\\') || (S.charAt(i) == ']')) )
+            {
+                NoPunc = NoPunc + S.charAt(i);
+            }
+            else if ( !((S.charAt(i) == '{') || (S.charAt(i) == '|') || (S.charAt(i) == '}')) )
+            {
+                NoPunc = NoPunc + S.charAt(i);
+            }
+        }
+        
+        return NoPunc;
     }
 }
